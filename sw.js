32e59e73cb5462646c33a538733c619e7708e985
xiaolajyc <72300443+xiaolajyc@users.noreply.github.com>
2026-09-15 01:59:57 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="house-maintenance-pwa-v26";
+const CACHE_NAME="house-maintenance-pwa-v27";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./dragon-repair.png","./app-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
